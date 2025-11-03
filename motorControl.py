9f70b70d02cbb9c196afe9c1b04da621ba216728
Ben <bbschiffer@gmail.com>
2025-11-02 20:40:38 -0500

    Working motor control

--- motorControl.py
+++ motorControl.py
@@ -1,43 +1,52 @@
 import time
 import RPi.GPIO as GPIO
 
-PUL_PIN = 26
-DIR_PIN = 12
+PU_pin = 13
+DR_pin = 11
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
 # Setup pins
-GPIO.setup(PUL_PIN, GPIO.OUT)
-GPIO.setup(DIR_PIN, GPIO.OUT)
+GPIO.setup(PU_pin, GPIO.OUT)
+GPIO.setup(DR_pin, GPIO.OUT)
 
 # Initialize pins
-GPIO.output(PUL_PIN, GPIO.LOW)
-GPIO.output(DIR_PIN, GPIO.HIGH)
+GPIO.output(PU_pin, GPIO.LOW)
+GPIO.output(DR_pin, GPIO.LOW)
 
 def moveMotor(distance, velocity):
     #400 pulses per revolution, 8mm per revolution
     
-    num_pulses = (abs(distance)/.008)*400;
+    rotations = abs(distance)/.008;
+    num_pulses = int(rotations*400);
     travel_time = abs(distance/velocity);
-    STEP_DELAY = travel_time/num_pulses;
+    step_delay = travel_time/num_pulses;
+    
+    #step_delay = .01
+    
+    #num_pulses = 50000
+    #print(exp_step_delay)
     
     print("Moving Gantry...")
     print("Distance:",distance,"m");
     print("Velocity:",velocity,"m/s");
+    print("Rotations:",rotations);
     print("Number of pulses:",num_pulses);
-    print("Pulse delay:",STEP_DELAY)
+    print("Pulse delay:",step_delay)
 
     try:
-        for i in num_pulses:
+        for i in range(int(num_pulses/2 + 1)):
+            GPIO.output(PU_pin, GPIO.HIGH)
+            time.sleep(step_delay)
+            #print("pulse",i)
+            GPIO.output(PU_pin, GPIO.LOW)
+            time.sleep(step_delay)
         
-            GPIO.output(PUL_PIN, GPIO.HIGH)
-            time.sleep(STEP_DELAY)
-            
-            GPIO.output(PUL_PIN, GPIO.LOW)
-            time.sleep(STEP_DELAY)
     except:
         print("Stopping motor...")
 
     finally:
         GPIO.cleanup()
         print("GPIO cleanup done.")
+
+#moveMotor(.008, 0.002) #One rotation at .002 m/s (4 second test)
